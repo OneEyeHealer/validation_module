f5736689a55019490116cf18a371e4db92d011f6
unknown <arihantjain812000@gmail.com>
2021-06-15 11:10:20 +0530

    added frontend template

---
+++ BackEnd/Scripts/jsFile/App.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("myApp",[]);
+app.controller("myController", function ($scope, $http) {
+   
+    $http.get("/api/Segment").then(function (d) {
+        $scope.data=d.data
+        console.log(d);
+    }, function (error) {
+            alert(d)
+    })
+    
+
+      
+})
