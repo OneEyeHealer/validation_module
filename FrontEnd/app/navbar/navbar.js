8854a40e6c744492ed7afdba55aad7bcaec29ce9
Harsh Arora <46344612+OneEyeHealer@users.noreply.github.com>
2021-06-15 11:29:59 +0530

    no need for different FrontEnd folder

--- FrontEnd/app/navbar/navbar.js
+++
@@ -1,8 +0,0 @@
-angular.module("myApp").component("navbar", {
-  templateUrl: "navbar/navbar.html",
-  controller:
-    ("$scope",
-    function NavBarController($scope) {
-      $scope.Title = "Task Maker";
-    }),
-});
